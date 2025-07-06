8b6e357f12f911d8b01ca008182989d51c41f50b
Ajay2023 <ajayn1803@gmail.com>
2025-07-06 16:39:26 +0530

    feat: MySQL inheritance using MappedSuper Class

---
+++ src/main/java/com/ajay/UberReviewService/entity/BaseModel.java
@@ -0,0 +1,29 @@
+package com.ajay.UberReviewService.entity;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+
+public abstract  class BaseModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected int id;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP) // This annotation tells spring about the formats of date object to be stored i.e. Date / Time / TimeStamp
+    @CreatedDate // This annotation tells Spring only handle it for Object Creation
+    protected Date createdAt;
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate // This annotation tells Spring only handle it for Object updation
+    protected Date updatedAt;
+
+}
--- src/main/java/com/ajay/UberReviewService/entity/Review.java
+++ src/main/java/com/ajay/UberReviewService/entity/Review.java
@@ -3,35 +3,20 @@ package com.ajay.UberReviewService.entity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-
-import java.util.Date;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @Table(name="bookingreview")
 @Data
 @Builder
-public class Review {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+
+public class Review extends  BaseModel {
 
     @Column(nullable = false)
     private String content;
 
     @Column
     private Double rating;
-
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP) // This annotation tells spring about the formats of date object to be stored i.e. Date / Time / TimeStamp
-    @CreatedDate // This annotation tells Spring only handle it for Object Creation
-    private Date createdAt;
-
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @LastModifiedDate // This annotation tells Spring only handle it for Object updation
-    private Date updatedAt;
 
     @Override
     public  String toString(){
